03954ced3720c7e66e05afb31ce98a124ff16d03
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-12 08:44:23 -0300

    Create web client

---
+++ web/src/app.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button';
+
+export function App() {
+  return (
+    <div>
+      <Button variant="secondary">Hello world</Button>
+      <Button variant="ghost">Hello world</Button>
+      <Button variant="link">Hello world</Button>
+      <Button variant="destructive">Hello world</Button>
+      <Button variant="link">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
